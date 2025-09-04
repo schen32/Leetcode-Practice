d1ba947748da05ee47927d3930cbc41d2c582ff1
Simon Chen <simonc8192@gmail.com>
2025-09-04 11:02:09 -0700

    392

---
+++ 392_IsSubsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s = deque(s)
+
+        for char in t:
+            if not s:
+                return True
+
+            if char == s[0]:
+                s.popleft()
+
+        return not s
